caadcccf326c8a8f67d280a1f003aed5ff164353
aieest <aie.estidola@gmail.com>
2024-08-28 11:38:51 +1200

    Added content to nav(temporary)

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,9 +1,18 @@
 const Nav = () => {
     return(
-        <nav>
-            Nav
+        <nav className="flex w-2/10">
+            <div className="nav-btn-div flex flex-col items-center justify-center">
+                <button className="up">Up</button>
+                <button className="down">Down</button>
+            </div>
+            <div className="nav-link-div">
+                <a href="" className="nav-link">Introduction</a>
+                <a href="" className="nav-link">About</a>
+                <a href="" className="nav-link">Projects</a>
+            </div>
         </nav>
     )
 }
 
 export default Nav;
+
